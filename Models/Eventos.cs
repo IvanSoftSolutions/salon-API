706feda2eaa58560b204041995ee69a64a1b0c1f
Ivan Hdz <ivan.daniel.hdz@gmail.com>
2022-03-10 12:37:50 -0600

    Fix Eventos model

--- Models/Eventos.cs
+++ Models/Eventos.cs
@@ -12,7 +12,7 @@ namespace salon_web_api.Models
 
         public int Id { get; set; }
         public int UserId { get; set; }
-        public DateOnly Fecha { get; set; }
+        public DateTime Fecha { get; set; }
         public string Estado { get; set; } = null!;
         public string Tipo { get; set; } = null!;
         public virtual Usuario User { get; set; } = null!;
